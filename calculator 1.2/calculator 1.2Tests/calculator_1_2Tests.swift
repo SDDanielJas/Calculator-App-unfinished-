5ca1e5b21c3d0df3253102fc38686967040e6a13
Daniel Jasinski <79961890+SDDanielJas@users.noreply.github.com>
2021-05-03 15:05:44 -0500

    Add files via upload

---
+++ calculator 1.2/calculator 1.2Tests/calculator_1_2Tests.swift
@@ -0,0 +1,33 @@
+//
+//  calculator_1_2Tests.swift
+//  calculator 1.2Tests
+//
+//  Created by Daniel Jasinski on 1/6/21.
+//
+
+import XCTest
+@testable import calculator_1_2
+
+class calculator_1_2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
